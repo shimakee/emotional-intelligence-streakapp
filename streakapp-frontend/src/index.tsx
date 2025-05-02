37b85cf6c9b211f272f400275e1c3c038f323822
Shimakee <shimakee@gmail.com>
2025-05-02 14:42:50 +0800

    feature/frontend: add react query and have call to backend streaks.

--- streakapp-frontend/src/index.tsx
+++ streakapp-frontend/src/index.tsx
@@ -4,17 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import Home from './pages/home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- streakapp-frontend/src/pages/streakcase/index.tsx
+++ streakapp-frontend/src/pages/streakcase/index.tsx
@@ -1,11 +1,55 @@
 import React from 'react';
 import './style.css';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
+enum StreakState {
+    INCOMPLETE = 'INCOMPLETE',
+    SAVED = 'SAVED',
+    AT_RISK = 'AT_RISK',
+    COMPLETED = 'COMPLETED',
+  }
+
+interface Day {
+    date: string;
+    activities: number;
+    state: StreakState; // INCOMPLETE | SAVED | AT_RISK | COMPLETED
+  }
+
+interface Streak {
+id: string;
+activitiesToday: number;
+total: number;
+days: Day[];
+}
+  
+  // Fetch function to get data from the API
+  const fetchData = async (streakcase: string | undefined): Promise<Streak[]> => {
+    if(streakcase === undefined || isNaN(Number(streakcase))) {
+        throw new Error('Invalid streakcase argument. Must be a number.');
+    }
+    
+    const response = await fetch(`http://localhost:8080/streaks/${streakcase}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  };
 
 const StreakCase: React.FC = () => {
     const { streakcase } = useParams<{streakcase?: string}>();
     const navigate = useNavigate();
+    const { data, error, isLoading, isError } = useQuery(
+        { queryKey: ['streakcase', streakcase], queryFn: () =>fetchData(streakcase) },  // Query Key and Query Function wrapped in an object
+      );
 
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error instanceof Error) {
+        return <p>Error: {error.message}</p>;
+    }
 
     if(streakcase === undefined || isNaN(Number(streakcase))) {
         navigate('/home');
@@ -15,6 +59,10 @@ const StreakCase: React.FC = () => {
     return (
             <div className="streakcase">
                 Streak case {+streakcase}
+
+                <p>
+                    {JSON.stringify(data)}
+                </p>
             </div>
 
         );
